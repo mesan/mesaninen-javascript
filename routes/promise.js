ff9672fb938f682138c3326dd4f96e365c8a18ef
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-08-25 14:43:27 +0200

    Set up endpoint for getting and posting a JSON resource.

--- routes/promise.js
+++ routes/promise.js
@@ -1,7 +1,28 @@
 module.exports = function(app) {
     app.get('/promise-exercise-1', promise1);
     app.get('/promise-exercise-2', promise2);
+    app.get('/message.json', messageGetter);
+    app.post('/message.json', messagePoster);
 };
+
+var message = { posted: new Date(), text: 'Hello!' };
+
+function messageGetter(req, res) {
+    res.json(message);
+}
+
+function messagePoster(req, res) {
+    if (!req.body || !req.body.text) {
+        return res.status(400).json('Please provide a message!');
+    }
+
+    message = {
+        posted: new Date(),
+        text: req.body.text
+    };
+
+    return res.json(message);
+}
 
 function promise1(req, res) {
     return res.render('promise_exercise_1', { pageTitle: 'Promises / Fetch - Oppgave 1', menu: 'promise-exercise-1' });
