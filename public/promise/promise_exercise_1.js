fba5ba1833598a0c3de12b98bd65904e1e3c47f1
Arild Wanvik Tvergrov <arildt@mesan.no>
2016-08-16 22:49:35 +0200

    Change innerHTML to innerText.

--- public/promise/promise_exercise_1.js
+++ public/promise/promise_exercise_1.js
@@ -3,4 +3,4 @@ const numbersElement = document.getElementById('numbers');
 const promisedNumbers = 
     new Promise(resolve => setTimeout(() => resolve(4815162342), 4000));
 
-promisedNumbers.then(numbers => numbersElement.innerHTML = numbers);
+promisedNumbers.then(numbers => numbersElement.innerText = numbers);
