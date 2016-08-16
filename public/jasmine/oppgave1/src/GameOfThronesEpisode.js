db954261b44f0f9a0d368500dcd541e5268bf40b
Arild Wanvik Tvergrov <arildt@mesan.no>
2016-08-16 23:11:53 +0200

    Resolve first assignment about Jasmine

--- public/jasmine/oppgave1/src/GameOfThronesEpisode.js
+++ public/jasmine/oppgave1/src/GameOfThronesEpisode.js
@@ -1,22 +1,34 @@
 function GameOfThronesEpisode(seasonNr, episodeNr, title) {
+    this.seasonNr = seasonNr;
+    this.episodeNr = episodeNr;
+    this.title = title;
+    this.peopleWillDie = false;
+    this.castMembers = [];
 }
 
 GameOfThronesEpisode.episodesInSeason = 10;
 
 GameOfThronesEpisode.prototype.printEpisode = function () {
+    return `S${this.seasonNr}E${this.episodeNr} - ${this.title}`;
 };
 
 GameOfThronesEpisode.prototype.containsCliffHanger = function (containsCliffHanger) {
+    this.peopleWillDie = containsCliffHanger;
 };
 
 GameOfThronesEpisode.prototype.calculateRemainingEpisodes = function () {
+    return GameOfThronesEpisode.episodesInSeason - this.episodeNr;
 };
 
 GameOfThronesEpisode.prototype.addCastMember = function (castMember) {
+    this.castMembers.push(castMember);
+    return this;
 };
 
 GameOfThronesEpisode.prototype.__defineGetter__('kicksAss', function () {
+    return this.castMembers.indexOf('Tyrion Lannister') > -1;
 });
 
 GameOfThronesEpisode.prototype.printCast = function () {
+    return this.castMembers.join(', ');
 };
